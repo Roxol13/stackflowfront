d9544183c5d879b89e90954a215d12c14c61e758
Tu Nombre <tucorreo@ejemplo.com>
2025-05-20 13:18:25 -0500

    pruebahp

--- src/App.js
+++ src/App.js
@@ -93,10 +93,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={isAuthenticated ? <Layout /> : <Login onLoginSuccess={handleLoginSuccess} />}
-        >
+        <Route        >
           <Route index element={<HomePage />} />
         </Route>
 
